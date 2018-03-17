7a470e637e922924f574d24c3725d73e4f9dc3eb
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-03-17 13:21:09 -0500

    Map working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { 
-        lat: -34.397, lng: 150.644 },
-        zoom: 8
+        lat: 5.8520, lng: -55.2038 },
+        zoom: 3
     });
 }
